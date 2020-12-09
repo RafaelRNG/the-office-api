a2c4bdf81ff89742cc8d57492cb5f8e80cfefde5
RafaelRNG <rafael.gomila@hotmail.com>
2020-12-09 20:28:39 -0300

    Criando Entity e Repository de Category

---
+++ src/main/java/com/rng/theofficeapi/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.rng.theofficeapi.repositories;
+
+import com.rng.theofficeapi.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
--- src/main/java/com/rng/theofficeapi/service/database/StartDatabase.java
+++ src/main/java/com/rng/theofficeapi/service/database/StartDatabase.java
@@ -1,6 +1,8 @@
 package com.rng.theofficeapi.service.database;
 
+import com.rng.theofficeapi.entities.Category;
 import com.rng.theofficeapi.entities.Salesman;
+import com.rng.theofficeapi.repositories.CategoryRepository;
 import com.rng.theofficeapi.repositories.SalesmanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,9 @@ public class StartDatabase {
     @Autowired
     private SalesmanRepository salesmanRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     public void database(){
         Salesman jim = new Salesman(null, "Jim Halpert", "34g3k1049fd04");
         Salesman pam = new Salesman(null, "Pam Beesly", "938278fduf2903");
@@ -20,6 +25,15 @@ public class StartDatabase {
         Salesman andy = new Salesman(null, "Andy Bernard", "9483742389fwefe");
         Salesman stanley = new Salesman(null, "Stanley Hudson", "rvguehi342");
 
+        Category sulfite = new Category(null, "Sulfite");
+        Category couche = new Category(null, "Couché");
+        Category reciclato = new Category(null, "Reciclato");
+        Category kraft = new Category(null, "Kraft");
+        Category vegetal = new Category(null, "Vegetal");
+        Category duoDesign = new Category(null, "Duo Design");
+        Category duplex = new Category(null, "Duplex");
+
         salesmanRepository.saveAll(Arrays.asList(jim, pam, dwight, andy, stanley));
+        categoryRepository.saveAll(Arrays.asList(sulfite, couche, reciclato, kraft, vegetal, duoDesign, duplex));
     }
 }
