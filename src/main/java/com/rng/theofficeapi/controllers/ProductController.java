c8868c4c3800e9a7ad69ba5955db311d3ac837a8
RafaelRNG <rafael.gomila@hotmail.com>
2020-12-16 03:13:58 -0300

    Criando os métodos para retornar todos, criar, alterar e deletar para Product

--- src/main/java/com/rng/theofficeapi/controllers/ProductController.java
+++ src/main/java/com/rng/theofficeapi/controllers/ProductController.java
@@ -4,10 +4,10 @@ import com.rng.theofficeapi.entities.Product;
 import com.rng.theofficeapi.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/products")
@@ -16,8 +16,34 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll(){
+        return ResponseEntity.ok(productService.findAll());
+    }
+
     @GetMapping(path = "/{id}")
     public ResponseEntity<Product> findById(@PathVariable Long id){
         return ResponseEntity.ok(productService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Product product){
+        productService.save(product);
+
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(product.getId()).toUri()).build();
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Product product){
+        productService.update(id, product);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        productService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/rng/theofficeapi/services/ProductService.java
+++ src/main/java/com/rng/theofficeapi/services/ProductService.java
@@ -4,7 +4,10 @@ import com.rng.theofficeapi.entities.Product;
 import com.rng.theofficeapi.repositories.ProductRepository;
 import com.rng.theofficeapi.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -12,7 +15,33 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    public List<Product> findAll(){
+
+        return productRepository.findAll();
+    }
+
     public Product findById(Long id){
-        return productRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found, ID: " + id));
+
+        return productRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException());
+    }
+
+    public void save(Product product){
+
+        productRepository.save(product);
+    }
+
+    public void update(Long id, Product newProduct){
+        Product oldProduct = this.findById(id);
+        newProduct.setId(oldProduct.getId());
+
+        productRepository.save(newProduct);
+    }
+
+    public void deleteById(Long id){
+        try{
+            productRepository.deleteById(id);
+        } catch(EmptyResultDataAccessException e){
+            throw new ObjectNotFoundException();
+        }
     }
 }
