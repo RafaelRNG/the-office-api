6ab97824c65149b76ee7cf02ca4ccf7ffb3fbe45
RafaelRNG <rafael.gomila@hotmail.com>
2020-12-18 11:54:05 -0300

    Criando os métodos para retornar todos, criar, alterar e deletar Client

--- src/main/java/com/rng/theofficeapi/controllers/ClientController.java
+++ src/main/java/com/rng/theofficeapi/controllers/ClientController.java
@@ -4,10 +4,11 @@ import com.rng.theofficeapi.entities.Client;
 import com.rng.theofficeapi.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/clients")
@@ -16,8 +17,35 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
+    @GetMapping
+    public ResponseEntity<List<Client>> findAll(){
+
+        return ResponseEntity.ok(clientService.findAll());
+    }
+
     @GetMapping(path = "/{id}")
     public ResponseEntity<Client> findById(@PathVariable Long id){
         return ResponseEntity.ok(clientService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Client client){
+        clientService.save(client);
+
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(client.getId()).toUri()).build();
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Client client){
+        clientService.update(id, client);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        clientService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/rng/theofficeapi/services/ClientService.java
+++ src/main/java/com/rng/theofficeapi/services/ClientService.java
@@ -6,13 +6,38 @@ import com.rng.theofficeapi.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClientService {
 
     @Autowired
     private ClientRepository clientRepository;
 
+    public List<Client> findAll(){
+
+        return clientRepository.findAll();
+    }
+
     public Client findById(Long id){
+
         return clientRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found, ID: " + id));
+    }
+
+    public void save(Client client) {
+
+        clientRepository.save(client);
+    }
+
+    public void update(Long id, Client newClient){
+        Client oldClient = this.findById(id);
+        newClient.setId(oldClient.getId());
+
+        clientRepository.save(newClient);
+    }
+
+    public void deleteById(Long id){
+
+        clientRepository.deleteById(id);
     }
 }
