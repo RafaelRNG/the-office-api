dad261736d2cf52cf19fa72af024001988ae9fc7
RafaelRNG <rafael.gomila@hotmail.com>
2020-12-21 01:41:09 -0300

    Criando método para retornar todos, criar, alterar e deletar para Salesman

--- src/main/java/com/rng/theofficeapi/controllers/SalesmanController.java
+++ src/main/java/com/rng/theofficeapi/controllers/SalesmanController.java
@@ -4,10 +4,10 @@ import com.rng.theofficeapi.entities.Salesman;
 import com.rng.theofficeapi.services.SalesmanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/salesmans")
@@ -16,8 +16,35 @@ public class SalesmanController {
     @Autowired
     private SalesmanService salesmanService;
 
+    @GetMapping
+    public ResponseEntity<List<Salesman>> findAll(){
+
+        return ResponseEntity.ok(salesmanService.findAll());
+    }
+
     @GetMapping(path = "/{id}")
     public ResponseEntity<Salesman> findById(@PathVariable Long id){
         return ResponseEntity.ok(salesmanService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Salesman salesman){
+        salesmanService.save(salesman);
+
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(salesman.getId()).toUri()).build();
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Salesman salesman){
+        salesmanService.update(id, salesman);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        salesmanService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/rng/theofficeapi/services/SalesmanService.java
+++ src/main/java/com/rng/theofficeapi/services/SalesmanService.java
@@ -4,7 +4,10 @@ import com.rng.theofficeapi.entities.Salesman;
 import com.rng.theofficeapi.repositories.SalesmanRepository;
 import com.rng.theofficeapi.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SalesmanService {
@@ -12,7 +15,32 @@ public class SalesmanService {
     @Autowired
     private SalesmanRepository salesmanRepository;
 
+    public List<Salesman> findAll(){
+
+        return salesmanRepository.findAll();
+    }
+
     public Salesman findById(Long id){
-        return salesmanRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found, ID: " + id));
+        return salesmanRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException());
+    }
+
+    public void save(Salesman salesman){
+
+        salesmanRepository.save(salesman);
+    }
+
+    public void update(Long id, Salesman newSalesman){
+        Salesman oldSalesman = this.findById(id);
+        newSalesman.setId(oldSalesman.getId());
+
+        salesmanRepository.save(newSalesman);
+    }
+
+    public void deleteById(Long id){
+        try{
+            salesmanRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ObjectNotFoundException();
+        }
     }
 }
